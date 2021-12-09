daf5e54070f9081ea755efc84df9ce5c50caa8a0
河儿 <60065901+jay-pro@users.noreply.github.com>
2021-12-09 23:50:06 +0700

    Add comments functionality

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -3,10 +3,29 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const articlesInfo = {
+  "learn-reat": {
+    comments: [],
+  },
+  "learn-node": {
+    comments: [],
+  },
+  "my-thoughts-on-learning-react": {
+    comments: [],
+  },
+};
+
 app.use(bodyParser.json());
 
-app.get("/hello", (req, res) => res.send("Hello"));
+/* app.get("/hello", (req, res) => res.send("Hello"));
 app.post("/hello", (req, res) => res.send(`Hello ${req.body.name}`));
-app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`));
+app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`)); */
+
+app.post("/api/articles/:name/add-comments", (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+  articlesInfo[articleName].comments.push({ username, text });
+  res.status(200).send(articlesInfo[articleName]);
+});
 
 app.listen(8000, () => console.log("Listening on port 8000"));
